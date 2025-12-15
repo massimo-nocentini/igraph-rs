be6bd9c7eab5daee8ceb053c2edd570caca04be7
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-12-15 16:41:31 +0100

    Remove unused build-dependencies and clean up Cargo.toml; add tests for igraph version in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,19 @@
 #![allow(non_snake_case)]
 
 include!("../bindings.rs");
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::mem;
+
+    #[test]
+    fn test_igraph_version() {
+        let major = IGRAPH_VERSION_MAJOR;
+        let minor = IGRAPH_VERSION_MINOR;
+        let patch = IGRAPH_VERSION_PATCH;
+        assert!(major == 1);
+        assert!(minor == 0);
+        assert!(patch == 0);
+    }
+}
